d6b6d1daf661102a002c2b7aba63ac27d3a1b76a
akashdhruv <akashdhruv@gwmail.gwu.edu>
2019-02-04 20:04:07 -0500

    errors in import poisson module

--- mae6225/poisson.py
+++ mae6225/poisson.py
@@ -1,15 +1,56 @@
 import numpy
 
 def getAnalytical(grid,asol):
+	for i in range(grid.nx+2):
+		for j in range(grid.ny+2):
+			grid.data[i,j,asol] = numpy.cos(2*numpy.pi*grid.x[i])*numpy.cos(2*numpy.pi*grid.y[j])
+	return
 
-    for i in range(grid.nx):
-	for j in range(grid.ny):
-    		grid.data[asol,i,j] = numpy.cos(2*numpy.pi*grid.x_cell[i])*numpy.cos(2*numpy.pi*grid.y_cell[j])
-
-def createRHS(grid,rvar):
+def getRHS(grid,rvar):
+	for i in range(grid.nx+2):
+		for j in range(grid.ny+2):
+			grid.data[i,j,rvar] = -8*numpy.pi*numpy.pi*numpy.cos(2*numpy.pi*grid.x[i])*numpy.cos(2*numpy.pi*grid.y[j])
+	return
 
 def solveJacobi(grid,ivar,rvar):
 
-def getError(grid,err,ivar,asol): 
+	max_iteration     = 5000
+	iteration_counter = 1
+	phi_old           = numpy.copy(grid.data[:,:,ivar])
 
-def applyBC:	
+	while iteration_counter < max_iteration:
+
+		grid.data[1:-1,1:-1,ivar] = ((phi_old[1:-1,2:]/(grid.dy**2))  + \
+		                             (phi_old[1:-1,:-2]/(grid.dy**2)) + \
+					     (phi_old[2:,1:-1]/(grid.dx**2))  + \
+				             (phi_old[:-2,1:-1]/(grid.dx**2)) - \
+				             grid.data[1:-1,1:-1,rvar])       * \
+                                             (1/((1/(grid.dx**2))             + \
+                                                 (1/(grid.dy**2))             + \
+                                                 (1/(grid.dx**2))             + \
+                                                 (1/(grid.dy**2))))
+
+		applyBC(grid.data[:,:,ivar])
+
+		residual = numpy.sum((grid.data[:,:,ivar]-phi_old)**2)/((grid.nx+2)*(grid.ny+2))
+	
+		if(residual < 0.000001 and residual != 0.0):
+			break
+
+		iteration_counter = iteration_counter + 1
+		phi_old           = numpy.copy(grid.data[:,:,ivar])
+
+	return iteration_counter, residual
+
+def getError(grid,eror,ivar,asol): 
+
+	return
+
+def applyBC(phi):	
+
+	phi[:,0]  = phi[:,1]
+	phi[:,-1] = phi[:,-2]
+	phi[0,:]  = phi[1,:]
+	phi[-1,:] = phi[-2,:]
+
+	return
