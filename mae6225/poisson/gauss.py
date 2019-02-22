ef469ee633b217b2f71ce0f0d91147a8660a44e1
alexuscamero <alexuscamero@gwu.edu>
2019-02-22 17:21:14 -0500

    gauss

--- mae6225/poisson/gauss.py
+++ mae6225/poisson/gauss.py
@@ -31,20 +31,22 @@ def solve_gauss(grid, ivar, rvar, maxiter=3000, tol=1e-9, verbose=False):
         
     """
     
-    i_ivar, i_rvar = grid.get_variable_indices([ivar, rvar])
+    i_ivar, i_rvar = grid.get_variable_indices(ivar, rvar)
     dx, dy = grid.dx, grid.dy
     
     ites = 0
     residual = tol + 1.0
     while ites < maxiter and residual > tol:
         phi_old = numpy.copy(grid.data[:, :, i_ivar])
-        for j in (1, grid.ny+1)
-            for i in (1, grid.nx+1)
-                grid.data[1:-1, 1:-1, i_ivar] = (((phi_old[:-2, 1:-1] + phi_old[2:, 1:-1]) * dy**2 +
-                                                  (phi_old[1:-1, :-2] + phi_old[1:-1, 2:]) * dx**2 -
-                                                               grid.data[1:-1, 1:-1, i_rvar] * dx**2 * dy**2) /
+        for j in range(1, grid.ny+1):
+            for i in range(1, grid.nx+1):
+                grid.data[i, j, i_ivar] = (((grid.data[i, j-1, i_ivar] + grid.data[i, j+1, i_ivar]) * dy**2 +
+                                                  (grid.data[i-1, j, i_ivar] + grid.data[i+1, j, i_ivar]) * dx**2 -
+                                                               grid.data[i, j, i_rvar] * dx**2 * dy**2) /
                                                          (2.0 * (dx**2 + dy**2)))
 
+                
+                
         grid.fill_guard_cells(ivar)
 
         residual = (numpy.sqrt(numpy.sum((grid.data[:, :, i_ivar] -
