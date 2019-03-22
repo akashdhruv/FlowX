9dafa895b24fb779a1acb23ed4013cf1ee134ccf
Elizabeth Gregorio <egregorio@gwu.edu>
2019-03-22 16:50:24 -0400

    corrected convective u operator

--- mae6225/ins/convective_u.py
+++ mae6225/ins/convective_u.py
@@ -5,25 +5,21 @@ def convective_u(gridx, gridy, ivar, cvar):
     idxY = gridy.get_variable_indices(ivar)
     idcX = gridx.get_variable_indices(cvar)
 
-    veluE = gridx.data[1:-1, 2:, idxX]
-    veluW = gridx.data[1:-1,:-2, idxX]
+    veluE = gridx.data[2:,  1:-1, idxX]
+    veluW = gridx.data[:-2, 1:-1, idxX]
+    veluP = gridx.data[1:-1,1:-1, idxX]
+    veluN = gridx.data[1:-1,  2:, idxX]
+    veluS = gridx.data[1:-1, :-2, idxX]
+   
+    velvNW = gridy.data[1:-2, 1:, idxY]
+    velvNE = gridy.data[2:-1, 1:, idxY]
+    velvSW = gridy.data[1:-2,:-1, idxY]
+    velvSE = gridy.data[2:-1,:-1, idxY]
     
-    velvN = gridy.data[2:, 1:-1, idxY]
-    velvS = gridy.data[:-2,1:-1, idxY]
-    velvE = gridy.data[1:-1, 2:, idxY]
-    velvW = gridy.data[1:-1,:-2, idxY]
-
-    veluP = (veluE + veluW) / 2
+    F = (-((veluP + veluE)**2 - (veluP + veluW)**2) / (4 * gridx.dx) - 
+          ((veluP + veluN)*(velvNE + velvNW)) / (4 * gridy.dy) - 
+          ((veluP + veluS)*(velvSE + velvSW)) / (4 * gridy.dy))
     
-    velvNW = (velvN + velvW) / 2
-    velvNE = (velvN + velvE) / 2
-    velvSW = (velvS + velvW) / 2
-    velvSE = (velvS + velvE) / 2
-
-    F[1:-1, 1:-1] = (-((veluP + veluE)**2 - (veluP veluW)**2) / 4 * gridx.dx - 
-                     ((veluP + veluN)*(velvNE + velvNW)) / 4 * gridy.dy - 
-                     ((veluP + veluS)*(velvSE + velvSW)) / 4 * gridy.dy)
-    
-    gridx.data[1:-1, 1:-1, idcX] = F[1:-1, 1:-1]
+    gridx.data[1:-1, 1:-1, idcX] = F
     
     return
