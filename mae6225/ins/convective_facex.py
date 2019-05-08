c9e900c6a45b24665114aa1494c6dddc8014f67a
mesnardo <mesnardo@gwu.edu>
2019-05-08 12:28:33 -0400

    Re-write convective routines

--- mae6225/ins/convective_facex.py
+++ mae6225/ins/convective_facex.py
@@ -21,25 +21,23 @@ def convective_facex(gridx, gridy, ivar):
         Convective terms in the x direction as an array of floats.
 
     """
-    fx = gridx.get_values(ivar)
-    fy = gridy.get_values(ivar)
+    u = gridx.get_values(ivar)
+    v = gridy.get_values(ivar)
+    dx, dy = gridx.dx, gridy.dy
 
-    dx = gridx.dx
-    dy = gridy.dy
+    u_P = u[1:-1, 1:-1]
+    u_W = u[:-2, 1:-1]
+    u_E = u[2:, 1:-1]
+    u_S = u[1:-1, :-2]
+    u_N = u[1:-1, 2:]
 
-    val_xE = fx[2:, 1:-1]
-    val_xW = fx[:-2, 1:-1]
-    val_xP = fx[1:-1, 1:-1]
-    val_xN = fx[1:-1, 2:]
-    val_xS = fx[1:-1, :-2]
+    v_sw = v[1:-2, :-1]
+    v_se = v[2:-1, :-1]
+    v_nw = v[1:-2, 1:]
+    v_ne = v[2:-1, 1:]
 
-    val_yNW = fy[1:-2, 1:]
-    val_yNE = fy[2:-1, 1:]
-    val_ySW = fy[1:-2, :-1]
-    val_ySE = fy[2:-1, :-1]
-
-    F = (-((val_xP + val_xE)**2 - (val_xP + val_xW)**2) / (4 * dx) -
-         (((val_xP + val_xN) * (val_yNE + val_yNW)) / (4 * dy) -
-          ((val_xP + val_xS) * (val_ySE + val_ySW)) / (4 * dy)))
+    F = - (((u_P + u_E)**2 - (u_W + u_P)**2) / (4 * dx) +
+           ((u_P + u_N) * (v_nw + v_ne) -
+            (u_S + u_P) * (v_sw + v_se)) / (4 * dy))
 
     return F
--- mae6225/ins/convective_facey.py
+++ mae6225/ins/convective_facey.py
@@ -17,48 +17,27 @@ def convective_facey(gridx, gridy, ivar):
 
     Returns
     -------
-    C : numpy.ndarray
+    F : numpy.ndarray
         Convective terms in the y direction as an array of floats.
 
     """
-    fx = gridx.get_values(ivar)
-    fy = gridy.get_values(ivar)
-
+    u = gridx.get_values(ivar)
+    v = gridy.get_values(ivar)
     dx, dy = gridx.dx, gridy.dy
-    nx, ny = gridy.nx, gridy.ny
 
-    C = numpy.zeros((nx + 2, ny + 1))
+    v_P = v[1:-1, 1:-1]
+    v_W = v[:-2, 1:-1]
+    v_E = v[2:, 1:-1]
+    v_S = v[1:-1, :-2]
+    v_N = v[1:-1, 2:]
 
-    for i in range(1, nx + 1):
-        for j in range(1, ny):
-            # Get stencil in x-dir
-            fxij = fx[i, j]
-            fxij1 = fx[i, j+1]
-            fxi_1j = fx[i - 1, j]
-            fxi_1j1 = fx[i - 1, j + 1]
+    u_sw = u[:-1, 1:-2]
+    u_se = u[1:, 1:-2]
+    u_nw = u[:-1, 2:-1]
+    u_ne = u[1:, 2:-1]
 
-            # Get stencil in y-dir
-            fyij = fy[i, j]
-            fyi1j = fy[i + 1, j]
-            fyij1 = fy[i, j + 1]
-            fyi_1j = fy[i - 1, j]
-            fyij_1 = fy[i, j - 1]
+    F = -(((u_se + u_ne) * (v_P + v_E) -
+           (u_sw + u_nw) * (v_W + v_P)) / (4 * dx) +
+          ((v_P + v_N)**2 - (v_S + v_P)**2) / (4 * dy))
 
-            # Read fx*fy value from the northeast-corner and northwest-corner
-            # on grid object
-            fxfy_ne = ((fxij + fxij1) / 2) * ((fyij + fyi1j) / 2)
-            fxfy_nw = ((fxi_1j + fxi_1j1) / 2) * ((fyij + fyi_1j) / 2)
-
-            # Read fy*fy value from the nort and south on grid object
-            fyfy_n = ((fyij + fyij1) / 2)**2
-            fyfy_s = ((fyij + fyij_1) / 2)**2
-
-            # Compute derivates
-            dfxfy_dx = (fxfy_nw - fxfy_ne) / dx
-            dfyfy_dy = (fyfy_n - fyfy_s) / dy
-
-            # Compute convection-v value in y-direction
-            C[i, j] = - dfxfy_dx - dfyfy_dy
-
-    # Return only internal values for C
-    return C[1:-1, 1:-1]
+    return F
