dcab67e727c866a54282548fc81120a6c1fb4d90
akashdhruv <akashdhruv@gwmail.gwu.edu>
2020-02-25 02:57:14 -0500

    updates for 2020 class

--- flowx/ins/__init__.py
+++ flowx/ins/__init__.py
@@ -1,6 +1,4 @@
 from .corrector import *
 from .divergence import *
 from .predictor import *
-from .predictor_AB2 import *
-from .predictor_RK3 import *
 from .stats import *
--- flowx/poisson/__init__.py
+++ flowx/poisson/__init__.py
@@ -1,4 +1,1 @@
-from .cg import *
-from .gauss import *
 from .jacobi import *
-from .sor import *
