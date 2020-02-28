540fa5f8631d96f89d289cfc405e9d67c3ba7431
akashdhruv <akashdhruv@gwmail.gwu.edu>
2020-02-28 14:56:41 -0500

    archictecture changes

--- flowx/ins/euler.py
+++ flowx/ins/euler.py
@@ -1,26 +1,29 @@
-from .corrector import corrector
-from .divergence import divergence
-from .predictor import predictor
+from .projection import predictor, corrector, divergence
 from .stats import stats
 
-def euler(gridc, gridx, gridy, scalars, predcorr):
+def euler(gridc, gridx, gridy, scalars, var_list, predcorr):
+
+    velc = var_list[0]
+    hvar = var_list[1]
+    divv = var_list[2]
+    pres = var_list[3]
 
     if(predcorr == 'predictor'):
 
         # Calculate predicted velocity: u* = dt*H(u^n)
-        predictor(gridx, gridy, 'velc', 'hvar', scalars.var['Re'], scalars.var['dt'])
+        predictor(gridx, gridy, velc, hvar, scalars.var['Re'], scalars.var['dt'])
     
         # Calculate RHS for the pressure Poission solver div(u)/dt
-        divergence(gridc, gridx, gridy, 'velc', 'divp', ifac = scalars.var['dt'])
+        divergence(gridc, gridx, gridy, velc, divv, ifac = scalars.var['dt'])
 
 
     elif(predcorr == 'corrector'):
 
         # Calculate corrected velocity u^n+1 = u* - dt * grad(P) 
-        corrector(gridc, gridx, gridy, 'velc', 'pres', scalars.var['dt'])
+        corrector(gridc, gridx, gridy, velc, pres, scalars.var['dt'])
     
         # Calculate divergence of the corrected velocity to display stats
-        divergence(gridc, gridx, gridy, 'velc', 'divc')
+        divergence(gridc, gridx, gridy, velc, divv)
     
         # Calculate stats
-        scalars.stats.update(stats(gridc, gridx, gridy, 'velc', 'pres', 'divc'))
+        scalars.stats.update(stats(gridc, gridx, gridy, velc, pres, divv))
