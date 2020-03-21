15435843d475fa9e83828beaf2aa96164afbd36a
akashdhruv <akashdhruv@gwmail.gwu.edu>
2020-03-21 05:13:43 -0400

    updates

--- flowx/io/plot_contour_mapped.py
+++ flowx/io/plot_contour_mapped.py
@@ -4,7 +4,7 @@ import numpy
 from matplotlib import pyplot
 
 
-def plot_contour(grid, var, levels=None):
+def plot_contour_mapped(grid, var, particle, levels=None):
     """Plot the filled contour of a variable on a meshgrid.
 
     Arguments
@@ -17,6 +17,8 @@ def plot_contour(grid, var, levels=None):
     """
     X, Y = numpy.meshgrid(grid.x, grid.y)
 
+    nodesC = numpy.vstack((particle.x[1:],particle.x[1]))
+
     pyplot.rc('font', family='serif', size=16)
     pyplot.figure()
     pyplot.xlabel('x')
@@ -26,9 +28,12 @@ def plot_contour(grid, var, levels=None):
         pyplot.contourf(X, Y, grid.get_values(var).transpose())
     else:
         pyplot.contourf(X, Y, grid.get_values(var).transpose(), levels)
+
+    pyplot.plot(nodesC[:,0], nodesC[:,1])
+    pyplot.plot(nodesC[:,0], nodesC[:,1], marker='.', markevery=5)
    
     pyplot.colorbar(label=var)
     pyplot.axis('scaled', adjustable='box')
     pyplot.xlim(X.min(), X.max())
     pyplot.ylim(Y.min(), Y.max())
-    #pyplot.show()
+    pyplot.show()
