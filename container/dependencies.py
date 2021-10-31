0d7e12ef7633103b8c98e40057c6a24e43c75394
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-10-31 18:47:59 -0400

    change depend script

--- container/dependencies.py
+++ container/dependencies.py
@@ -3,13 +3,12 @@ import os
 
 basedir = os.getenv('PWD')+'/..'
 
-# Build container
-flowx = pymaple.Maple(image='akashdhruv/flowx:latest',container='flowx_latest',
+# Build dependencies inside the container
+flowx = pymaple.Maple(image='akashdhruv/flowx:latest',container='flowx',
                       source=basedir,target='/home/mount/flowx')
 
 flowx.build()
 flowx.pour()
-flowx.execute('"cd tools/bubblebox && \
-                ./setup develop && ./setup clean"')
+flowx.execute('"cd tools/bubblebox && ./setup develop && ./setup clean"')
 flowx.commit()
 flowx.rinse()
