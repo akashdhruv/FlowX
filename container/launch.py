c46884228862ae28fe5e0838e4762da77b2bfdce
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2021-11-23 22:49:18 -0500

    update flowx

--- container/launch.py
+++ container/launch.py
@@ -2,17 +2,20 @@ import pymaple
 import os
 
 if __name__ == "__main__":
-    """ 
     """
-    # Set base directory
-    basedir = os.getenv('PWD')+'/..'
+    """
+    # Set basedir
+    basedir = os.getenv('PWD') + '/..'
 
     # Create a container for the application
     flowx = pymaple.Maple(image='akashdhruv/flowx:latest',container='flowx',
                           source=basedir,target='/home/mount/flowx')
-
     flowx.build()
     flowx.pour()
     flowx.execute('cd tools/bubblebox && ./setup develop && ./setup clean')
-    flowx.commit()
+
+    flowx.execute('"./setup develop && ./setup clean && python3 tests/container && \
+                     /home/user/.local/bin/jupyter notebook --port=8888 --no-browser --ip=0.0.0.0"')
     flowx.rinse()
+    flowx.clean()
+    flowx.remove()
