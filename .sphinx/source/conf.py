4df227f21c4a5e32bde1fc090e7bb33f0fce70fd
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-01-21 17:33:58 -0500

    update docs

--- .sphinx/source/conf.py
+++ .sphinx/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../flowx'))
+sys.path.insert(0, os.path.abspath('../../FlowX'))
 
 # -- Project information -----------------------------------------------------
 
--- docs/_sources/index.rst.txt
+++ docs/_sources/index.rst.txt
@@ -1,3 +1,4 @@
+
 .. FlowX documentation master file, created by
    sphinx-quickstart on Thu Jan 20 23:56:36 2022.
    You can adapt this file completely to your liking, but it should at least
